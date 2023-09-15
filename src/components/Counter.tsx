0f6a8e81397c7757e9f1b5c3a8a0ab8a4fdcd50f
Miguel Ángel Durán <miduga@gmail.com>
2023-09-15 17:51:31 +0200

    Add bun project

---
+++ src/components/Counter.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react"
+
+export const Counter = () => {
+  const [count, setCount] = useState(0)
+
+  const increment = () => setCount(count + 1)
+  const decrement = () => setCount(count - 1)
+
+  return (
+    <div>
+      <p>Count: 0</p>
+      <button onClick={increment}>+</button>
+      <button onClick={decrement}>-</button>
+    </div>
+  )
+}
---
+++ src/index.tsx
@@ -0,0 +1,40 @@
+import { Elysia, t } from "elysia";
+import { html } from "@elysiajs/html"
+import { swagger } from '@elysiajs/swagger'
+import { Counter } from "./components/Counter";
+
+const TODOS = [
+  { id: 1, title: 'Buy milk' },
+  { id: 2, title: 'Buy eggs' },
+  { id: 3, title: 'Buy bread' }
+]
+
+const app = new Elysia()
+  .use(html())
+  .use(swagger())
+  .get("/", () => "Hello Elysia", {
+    response: t.String()
+  })
+  .get('/todos', () => TODOS, {
+    detail: {
+      summary: 'Return all TODOs for the user',
+      tags: ['todos']
+    },
+    response: t.Array(t.Object({
+      id: t.Number(),
+      title: t.String()
+    }))
+  })
+  .get('/todos/:id', ({ params: { id } }) => {
+    return TODOS.find(todo => todo.id === +id)
+  }, {
+    params: t.Object({
+      id: t.Number()
+    }),
+  })
+  .get('/app', () => <Counter />)
+  .listen(3000);
+
+console.log(
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+);
