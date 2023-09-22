f9e68663e4971b1348f7bff39bc6c76d30e5dd9c
Gabriel Trabanco Llano <gtrabanco@users.noreply.github.com>
2023-09-22 13:14:23 +0200

    Delete the style because it does not exists

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ export default function IndexPage() {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="stylesheet" href="/styles.css"></link>
+        {/* <link rel="stylesheet" href="/styles.css"></link> */}
         <title>My app</title>
       </head>
       <body>
